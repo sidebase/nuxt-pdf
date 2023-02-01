a96958872e83165f7c40ac156cb11e6d573f51d2
Zoey <zoeykaiser8@gmail.com>
2023-02-01 16:57:00 +0100

    feat: rewrite with `jsPDF` (#6)
    * Added new wrapper and cleaned up playground
    
    * Updated Playground to be more comprehensive
    
    * Removed plugin, added composables
    
    * Added protected page and small improvements
    
    * Cleaned up playground and module
    
    * Fixed linting error
    
    * Fixed linting error
    
    * Renamed composable and slight improvements
    
    * Updated Docs
    
    * Fixed License file
    
    * Small fix in docs

---
+++ playground/app.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div style="position: fixed">
+      <button
+        style="border-radius: 0; font-size: 20px; cursor: pointer"
+        @click="exportToPDF('pdf_export.pdf', pdfSection, undefined, {html2canvas: {scale: 0.7, useCORS: true}})"
+      >
+        Generate normal PDF
+      </button>
+      <br>
+      <button
+        style="border-radius: 0; font-size: 20px; cursor: pointer; margin-top: 10px"
+        @click="printProtected(pdfSection)"
+      >
+        Generate protected PDF
+      </button>
+    </div>
+    <div ref="pdfSection" style="width: 500px; margin: auto">
+      <TestingStage />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { exportToPDF } from '#imports'
+
+const pdfSection = ref<HTMLElement | null>(null)
+
+const printProtected = (HTMLElement: HTMLElement | undefined) => {
+  exportToPDF('pdf_protected_export.pdf', HTMLElement,
+    {
+      encryption: {
+        ownerPassword: 'test',
+        userPassword: 'test2',
+        userPermissions: ['print']
+      }
+    }, {
+      html2canvas: {
+        scale: 0.7,
+        useCORS: true
+      }
+    })
+}
+</script>
+
+<style>
+html {
+  padding: 0
+}
+body {
+  padding: 0;
+  margin: 0;
+}
+</style>
