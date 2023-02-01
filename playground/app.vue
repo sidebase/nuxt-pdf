f32cb6e70d4bb2908e9109459c46b66d953b8827
Zoey Kaiser <zoeykaiser8@gmail.com>
2023-02-01 15:07:45 +0100

    Cleaned up playground and module

---
+++ playground/app.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div style="position: fixed">
+      <button
+        style="border-radius: 0; font-size: 20px; cursor: pointer"
+        @click="usePDFExport(pdfSection, undefined, {html2canvas: {scale: 0.7, useCORS: true}})"
+      >
+        Generate normal PDF
+      </button><br />
+      <button
+        style="border-radius: 0; font-size: 20px; cursor: pointer; margin-top: 10px"
+        @click="printProtected(pdfSection)"
+      >
+        Generate protected PDF
+      </button>
+    </div>
+    <div ref="pdfSection" style="width: 500px; margin: auto">
+      <TestingStage />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { usePDFExport } from '#imports'
+
+const pdfSection = ref<HTMLElement | null>(null)
+
+const printProtected = (HTMLElement: HTMLElement | undefined) => {
+  usePDFExport(HTMLElement,
+    {
+      encryption: {
+        ownerPassword: 'test',
+        userPassword: 'test2',
+        userPermissions: ['print']
+      }
+    }, {
+      html2canvas: {
+        scale: 0.7,
+        useCORS: true
+      }
+    })
+}
+</script>
+
+<style>
+html {
+  padding: 0
+}
+body {
+  padding: 0;
+  margin: 0;
+}
+</style>
