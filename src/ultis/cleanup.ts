2916c180c840cc50f462dccef6a2126c56255d5c
Zoey Kaiser <zoeykaiser8@gmail.com>
2024-02-05 18:50:23 +0100

    added useRender composable

---
+++ src/ultis/cleanup.ts
@@ -0,0 +1,16 @@
+export default function cleanup (str: string) {
+  if (!str || typeof str !== 'string') {
+    return str
+  }
+
+  return str
+    .replace(/ data-v-inspector="[^"]*"/g, '')
+    .replace(/<!--\[-->/g, '')
+    .replace(/<!--]-->/g, '')
+    .replace(/<template>/g, '')
+    .replace(/<template[^>]*>/g, '')
+    .replace(/<\/template>/g, '')
+    .replace(/<clean-component>/g, '')
+    .replace(/<clean-component[^>]*>/g, '')
+    .replace(/<\/clean-component>/g, '')
+}
