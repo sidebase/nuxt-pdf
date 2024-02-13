10b44670f39d8f73799348c6fafce5ac7ef12546
Zoey Kaiser <zoeykaiser8@gmail.com>
2024-02-13 03:11:40 +0100

    removed devTool from moduleOptions

--- src/module.ts
+++ src/module.ts
@@ -6,7 +6,6 @@ const PACKAGE_NAME = '@sidebase/nuxt-pdf'
 
 // Module options TypeScript interface definition
 const defaultOptions: ModuleOptions = {
-  devtools: false,
   defaultDocOptions: {
     size: 'A4',
     bufferPages: true,
@@ -45,9 +44,5 @@ export default defineNuxtModule<ModuleOptions>({
       )
       nitroConfig.alias['#pdf'] = resolver.resolve('./runtime/server')
     })
-
-    if (options.devtools) {
-      setupDevToolsUI(nuxt, resolver)
-    }
   }
 })
